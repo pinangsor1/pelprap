3bd408aae69e16b36e9a33f648a044cf1b117378
Yesa <79618650+pinangsor1@users.noreply.github.com>
2023-09-19 14:57:57 +0700

    deploy pertama
    deploy pertama

---
+++ script.js
@@ -0,0 +1,13 @@
+var menuBtn = document.getElementsByClassName("burger-menu")
+var sideNav = documen.getElementsByClassName("line")
+
+burger-menu.style.right == "-350px"
+
+menuBtn.onclick = function() {
+	if(burger-menu.style.right == "-250px"){
+		burger-menu.style.right = "0";
+	}
+	else{
+		burger-menu.style.right = "-250px";
+	}
+}
